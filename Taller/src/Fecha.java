16c7080390e049c70e9e0c1bc819ac503cc0551a
Angell Pimienta's <angelefrain1510@gmail.com>
2024-03-18 17:04:25 -0500

    initial commit

---
+++ Taller/src/Fecha.java
@@ -0,0 +1,39 @@
+public class Fecha {
+    private short dd;
+    private short mm;
+    private short aa;
+
+    public Fecha(){
+
+    }
+
+    public Fecha (short dd, short mm, short aa) {
+        this.dd = dd;
+        this.mm = mm;
+        this.aa = aa;
+    }
+
+    public void setDia(short dd) {
+        this.dd = dd;
+    }
+    public void setMes(short mm) {
+        this.mm = mm;
+    }
+    public void setA(short aa) {
+        this.aa = aa;
+    }
+
+    public short getDia(){
+        return dd;
+    }
+    public short getMes(){
+        return mm;
+    }
+    public short getA(){
+        return aa;
+    }
+    public String toString (){
+        return "Fecha de nacimiento: \n"  + dd + "-" + mm + "-" + aa;
+    }
+}
+
