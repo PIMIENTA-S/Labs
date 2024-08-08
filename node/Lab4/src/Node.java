4bd97d2aef359c6590a078f97b89d19acb33104e
Angell Pimienta's <angelefrain1510@gmail.com>
2024-08-08 09:46:07 -0500

    new Node

--- node/Lab4/src/Node.java
+++ node/Lab4/src/Node.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class node {
+public class Node {
+
+    private Object data;
+    private Node next;
+
+
+    public Node(){
+        Node a = new Node();
+        a.setNext(null);
+        a.setData(null);
+    }
+
+    public Node(Object e){
+        Node nuevo = new Node();
+        nuevo.setData(e);
+        nuevo.setNext(null);
+
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
