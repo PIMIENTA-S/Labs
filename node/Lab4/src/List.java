4e84a08bb324245f9dc2726825dba81eeb402769
Angell Pimienta's <angelefrain1510@gmail.com>
2024-08-08 10:21:30 -0500

    add funtion

--- node/Lab4/src/List.java
+++ node/Lab4/src/List.java
@@ -44,20 +44,53 @@ public class List {
             head = n;
         }
         size++;
-
     }
 
     public void addLast(Object e){
-
-        
+        Node n =  new Node(e);
+        if (isEmpty()){
+            head = n;
+            tail = n;
+        } else{
+            tail.setNext(n);
+            tail = n;
+        }
+        size++;
 
     }
 
-    public void removeFirst(){ //retorna un object
+    public Object removeFirst(){ //retorna un object
+        if (!isEmpty()){
+            Node temp = head;
+            // reescribimos el enlace al siguiente nodo
+            head = temp.getNext();
+            temp.setNext(null); // quitamos la conexion del primer nodo al sgt
+            size --;
+
+            return temp.getData();
+        }else{
+            return null;
+        }
 
     }
+    public Object removeLast(){
 
-    public void removeLast(){
+        if (size == 1){
+            return removeFirst();
+        } else{
+            Node temp = tail;
+            Node anterior = head;
+            //cuando anterior sea igual al tail, quedara con el valor anterior
+            while (anterior.getNext() != tail){
+                anterior = anterior.getNext();
+            }
+            //desconectamos el penultimo elemento del ultimo con (null)
+            anterior.setNext(null);
+
+            tail = anterior;
+            size --;
+            return temp.getData();
+        }
 
     }
 }
