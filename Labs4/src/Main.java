2a15c5e5e2cba609161c296799f06fd0e482fcae
Angell Pimienta's <angelefrain1510@gmail.com>
2024-08-13 01:28:45 -0500

    add testing

--- Labs4/src/Main.java
+++ Labs4/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        DoubleList prueba = new DoubleList();
+
+        prueba.addLast("Testing 1");
+        prueba.addLast("Testing 2");
+        prueba.addLast("Testing 3");
+        prueba.addLast("Testing 4");
+        prueba.addLast("Testing 5");
+        prueba.addLast("Testing 6");
+
+        Object e = "Angell";
+        DoubleNode p1 = new DoubleNode(e);
+
+
+
+        prueba.showList();
     }
 }
