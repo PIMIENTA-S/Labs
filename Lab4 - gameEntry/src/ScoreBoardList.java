47ef7fd5e27a1ca340ecb691831cc574bac7b6b9
Angell Pimienta's <angelefrain1510@gmail.com>
2024-08-14 10:06:48 -0500

    add structure

--- Lab4 - gameEntry/src/ScoreBoardList.java
+++ Lab4 - gameEntry/src/ScoreBoardList.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
-
 public class ScoreBoardList {
+
+    private List board;
+
+    public ScoreBoardList(){
+
+    }
+
+    public void add(GameEntry e){
+        /* Se crea un nodo auxiliar */
+        Node n = new Node(e);
+        if (board.isEmpty()){
+            board.addFirst(e);
+        } else{
+            Node temp = board.First();
+            //while (temp != null && temp.getData() < e.getScore()){
+
+        }
+
+    }
+
+    public void remove(String n){
+
+    }
+
 }
