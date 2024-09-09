9162b1a182d230c21772f9efc9811a7262741300
Angell Pimienta's <angelefrain1510@gmail.com>
2024-09-09 01:31:08 -0500

    testing bst+

--- Lab8/src/bst.java
+++ Lab8/src/bst.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class bst {
+
+    private Object data;
+    private int k;
+
+
+    public bst(Object e, int k){
+        data = e ;
+        this.k = k;
+    }
+
+    public void setData(Object d){
+        data = d ;
+    }
+
+    public void setKey(int k){
+        this.k  = k ;
+    }
+
+    public Object getData(){
+        return data ;
+    }
+
+    public int getKey(){
+        return this.k ;
+    }
+
+    @Override
+    public String toString() {
+        return "bst{" +
+                "data=" + data +
+                ", k=" + k +
+                '}';
+    }
 }
