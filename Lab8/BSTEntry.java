0ecbc4835790d451ca0d036c10ab5c5ece73effd
Angell Pimienta's <angelefrain1510@gmail.com>
2024-10-13 21:35:58 -0500

    remane folder

--- Lab8/BSTEntry.java
+++ Lab8/BSTEntry.java
@@ -1,10 +1,10 @@
-public class bst {
+public class BSTEntry extends BinaryTree {
 
     private Object data;
     private int k;
+    
 
-
-    public bst(Object e, int k){
+    public BSTEntry(Object e, int k){
         data = e ;
         this.k = k;
     }
@@ -12,24 +12,25 @@ public class bst {
     public void setData(Object d){
         data = d ;
     }
-
+    
     public void setKey(int k){
         this.k  = k ;
     }
-
+    
     public Object getData(){
         return data ;
     }
-
+    
     public int getKey(){
         return this.k ;
-    }
+    };
 
     @Override
     public String toString() {
-        return "bst{" +
-                "data=" + data +
-                ", k=" + k +
-                '}';
+        return data +", k=" + k;
     }
+
+
+
+    
 }
